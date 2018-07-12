50877ffb6d7555f08b51a563c33322543c467e74
trurtrl <trurtrl@gmail.com>
2018-07-12 20:29:48 +0300

    Add BP PlayerPawn (based on SCharacter) and Camera to it (in CPP)

--- Source/CoopGame/Private/SCharacter.cpp
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -2,6 +2,7 @@
 
 #include "SCharacter.h"
 #include "Runtime/Engine/Classes/Components/InputComponent.h"
+#include "Runtime/Engine/Classes/Camera/CameraComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -9,6 +10,10 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	
+	//	lets rotate camera
+	CameraComp->bUsePawnControlRotation = true;
 }
 
 // Called when the game starts or when spawned
--- Source/CoopGame/Public/SCharacter.h
+++ Source/CoopGame/Public/SCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "SCharacter.generated.h"
 
+class UCameraComponent;
+
 UCLASS()
 class COOPGAME_API ASCharacter : public ACharacter
 {
@@ -16,12 +18,16 @@ public:
 	ASCharacter();
 
 protected:
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	void MoveForward(float Value);
 
 	void MoveRight(float Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent * CameraComp;
 
 public:	
 	// Called every frame
