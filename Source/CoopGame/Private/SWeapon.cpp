9846b43c09e744fdd85d7956f5474a3d0c904891
trurtrl <trurtrl@gmail.com>
2018-07-14 11:19:00 +0300

    Create Debug Console Variables ( now if put in console "COOP.DebugWeapons 1" debug lines will be drawn, and if put "COOP.DebugWeapons 0" they won't )

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -8,6 +8,9 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Particles/ParticleSystemComponent.h"
 
+static int32 DebugWeaponDrawing = 0;
+FAutoConsoleVariableRef CVARDebugWeaponDrawing(TEXT("COOP.DebugWeapons"), DebugWeaponDrawing, TEXT("Draw Debug Lines for Weapons"), ECVF_Cheat);
+
 // Sets default values
 ASWeapon::ASWeapon()
 {
@@ -75,7 +78,10 @@ void ASWeapon::Fire()
 			TracerEndPoint = Hit.ImpactPoint;
 		}
 
-		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		if (DebugWeaponDrawing > 0)
+		{
+			DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		}
 
 		if (MuzzleEffect)
 		{
