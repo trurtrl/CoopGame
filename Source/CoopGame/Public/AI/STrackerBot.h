7467026022de36e8b8d97e653586f0fa0086b175
trurtrl <trurtrl@gmail.com>
2018-08-17 10:43:23 +0300

    CHALLENGE: Increase AI Damage when near other bots

--- Source/CoopGame/Public/AI/STrackerBot.h
+++ Source/CoopGame/Public/AI/STrackerBot.h
@@ -88,5 +88,19 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+protected:
+
+	// CHALLENGE CODE	
+
+	// Find nearby enemies and grow in 'power level' based on the amount.
+	void OnCheckNearbyBots();
+
+	// the power boost of the bot, affects damaged caused to enemies and color of the bot (range: 1 to 4)
+	int32 PowerLevel;
+
+	FTimerHandle TimerHandle_RefreshPath;
+
+	void RefreshPath();
 	
 };
